d3d308e751d825c29edf7960bd8020101fb9b10f
JarandDev <jarand@jarand.dev>
2021-05-09 10:30:12 +0200

    #1: Get ChatMessage.id from Twitch tag
    Fixes #1

--- src/main/java/dev/j4d/twitchchatreader/event/assembler/ChatMessageEventV1Assembler.java
+++ src/main/java/dev/j4d/twitchchatreader/event/assembler/ChatMessageEventV1Assembler.java
@@ -19,22 +19,22 @@ public class ChatMessageEventV1Assembler {
 
     public ChatMessageEventV1 assemble(RawEvent rawEvent) {
         final var inputParts = rawEvent.getRawData().split(" ");
-        final var chatMetadata = tagsAssembler.assemble(inputParts[0]);
+        final var tags = tagsAssembler.assemble(inputParts[0]);
         final var messageBuilder = new StringBuilder();
         for (int i = 4; i < inputParts.length; i++) {
             messageBuilder.append(inputParts[i]).append(" ");
         }
         return new ChatMessageEventV1(
-                UUID.fromString(rawEvent.getMetadata().getEventId()),
+                UUID.fromString(tags.get("id")),
                 inputParts[1].split("!")[0].substring(1),
-                chatMetadata.get("display-name"),
+                tags.get("display-name"),
                 messageBuilder.substring(1).trim(),
                 inputParts[3].substring(1),
-                Long.parseLong(chatMetadata.get("user-id")),
-                Long.parseLong(chatMetadata.get("room-id")),
-                chatMetadata.get("mod").equals("1"),
-                chatMetadata.get("subscriber").equals("1"),
-                chatMetadata.get("turbo").equals("1"),
+                Long.parseLong(tags.get("user-id")),
+                Long.parseLong(tags.get("room-id")),
+                tags.get("mod").equals("1"),
+                tags.get("subscriber").equals("1"),
+                tags.get("turbo").equals("1"),
                 Instant.parse(rawEvent.getMetadata().getCreated()).truncatedTo(ChronoUnit.SECONDS).toString());
     }
 }
